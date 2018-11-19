c418c04663d11f036d1415c0a9f5ef828059aa4f
yvesenergy <yvesenergy@gmail.com>
2018-11-19 08:55:55 +0300

    landing

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def social
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/social'
+  get 'home/about'
+  get 'home/contact'
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
